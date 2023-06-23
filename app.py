9fcc3115e812af760102745bb30eab473b638281
Raffaele Spataro <raphael2692@gmail.com>
2023-06-23 16:54:30 +0200

    simplified ui, streaming mode, faster

--- app.py
+++ app.py
@@ -1,95 +1,85 @@
 import os
 import json
+
 import yaml
+import pyfiglet
+
 from langchain.memory import ConversationBufferMemory
 from langchain import LLMChain, PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from rich.console import Console
-from rich.text import Text
-from rich.prompt import Prompt
-from rich.markdown import Markdown
-import pyfiglet
-
-
-# helper functions to pretty print in the console
-def rich_print(role: str, text: str):
-    if role == 'system':
-        console.print(Text("SYSTEM: ", style="bold yellow"),  Markdown(text))
-    elif role == 'bot':
-        console.print(Text("BOT: ", style="bold blue"),  Markdown(text))
-    elif role == 'user':
-        console.print(Text("User: ", style="bold green"),  Markdown(text))
-
-def rich_input(role: str = 'user') -> str:
-    if role == 'user':
-        return Prompt.ask("[bold green]USER")
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 # read openai api key and model configuration
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "secrets.json"), "r") as jsonfile:
+with open(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "secrets.json"), "r"
+) as jsonfile:
     secrets = json.load(jsonfile)
     os.environ["OPENAI_API_KEY"] = secrets["openai_key"]
 
 # raed chat settings
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.yaml"), "r") as yamlfile:
+with open(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.yaml"), "r"
+) as yamlfile:
     config = yaml.safe_load(yamlfile)
 
 # read chat templates
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates.yaml"), "r") as yamlfile:
+with open(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates.yaml"), "r"
+) as yamlfile:
     templates = yaml.safe_load(yamlfile)
 
 if __name__ == "__main__":
-    # init rich console session
-    console = Console()
-    
-    # welcome screen
-    welcome_message = pyfiglet.figlet_format("Command Line ChatGPT", font='rectangles')
-    console.print(Text(welcome_message, style="bold yellow on black"))
+    welcome_message = pyfiglet.figlet_format("Command Line ChatGPT", font="rectangles")
+    print(welcome_message)
 
-    # prompt template picking loop
     while True:
-        
         try:
             templates_list = "\n".join([f"* {t}" for t in templates.keys()])
-            rich_print('system', 'Select one of the templates below or type \'quit\' to exit:')
-            print(templates_list)
-            template_choice = rich_input()
-            
+            print("system: Select one of the templates below or type 'quit' to exit:")
+            print(f"\n{templates_list}\n")
+            template_choice = input("user: ")
+
             if template_choice == "quit":
-                rich_print('system', 'Goodbye!')
+                print("system: Goodbye!")
                 break
 
             if template_choice not in templates:
-                rich_print('system', 'Invalid template choice')
-                continue 
-        
+                print("system: Invalid template choice")
+                continue
+
         except KeyboardInterrupt:
             break
 
-        # initialize conversation
-        rich_print('system', f'You have chosen \"{template_choice}\" template. Start prompting or type \'quit\' to exit:')
-        prompt = PromptTemplate(
-            input_variables=["chat_history", "human_input"], template=templates[template_choice]
+        print(
+            f"system You have chosen \"{template_choice}\" template. Start prompting or type 'quit' to exit:"
         )
 
-        # use memory
-        memory = ConversationBufferMemory(memory_key="chat_history")
-
         llm_chain = LLMChain(
-            llm=ChatOpenAI(model_name=config["model_name"], temperature=config["temperature"], max_tokens=config["max_tokens"]),
-            prompt=prompt,
+            llm=ChatOpenAI(
+                model_name=config["model_name"],
+                temperature=config["temperature"],
+                max_tokens=config["max_tokens"],
+                streaming=True,
+                callbacks=[StreamingStdOutCallbackHandler()],
+            ),
+            prompt=PromptTemplate(
+                input_variables=["chat_history", "human_input"],
+                template=templates[template_choice],
+            ),
             verbose=False,
-            memory=memory,
+            memory=ConversationBufferMemory(memory_key="chat_history"),
         )
 
         # conversation loop
         while True:
             try:
-                human_input = rich_input()
+                print("")
+                human_input = input("user: ")
                 if human_input == "quit":
-                    rich_print('system', 'Quitting conversation...')
+                    print("system: Quitting conversation...")
                     break
                 else:
-                    response = llm_chain.predict(human_input=human_input)
-                    rich_print('bot', f'{response}')
+                    print("bot: ")
+                    llm_chain.predict(human_input=human_input)
             except KeyboardInterrupt:
                 break
